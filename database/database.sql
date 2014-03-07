66d1bcf2739a402d32701bac120404cb9959a2a1
Sam Hipwell <samhipwell@gmail.com>
2014-03-07 15:13:12 +0000

    Deleting old file that are not needed. And adding work what I have done so far on the database.

---
+++ database/database.sql
@@ -0,0 +1,94 @@
+CREATE TABLE Users (
+	Users_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	Name VARCHAR(255) NOT NULL,
+	Money INTEGER
+);
+
+CREATE TABLE HightScore (
+	HightScore_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	Users_id INTEGER,
+	Score INTEGER,
+	CONSTRAINT fk_High_Users FOREIGN KEY (Users_id) REFERENCES Users (Users_id)
+);
+
+CREATE TABLE Ship (
+	Ship_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	Model INTEGER,
+	Ammo_Level INTEGER,
+	Health_Level INTEGER,
+	Cargo_Level INTEGER,
+	Fuel_Level INTEGER,
+	Owner INTEGER,
+	Extenstions VARCHAR(255),
+	x_loc INTEGER,
+	y_loc INTEGER,
+	CONSTRAINT fk_ship_Users FOREIGN KEY (Owner) REFERENCES Users (Users_id)
+);
+
+CREATE TABLE Resources (
+	Resources_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	Name VARCHAR(255) NOT NULL,
+	Initial_Price INTEGER,
+	Description VARCHAR(255)
+);
+
+CREATE TABLE Planet (
+	Planet_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	Title VARCHAR(255),
+	X_loc INTEGER,
+	Y_loc INTEGER,
+	Diameter INTEGER
+);
+
+CREATE TABLE Media (
+	Media_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	X_size INTEGER,
+	Y_size INTEGER,
+	length INTEGER,
+	file_Loc VARCHAR(255),
+	Media_type INTEGER
+);
+
+CREATE TABLE PlanetResources (
+	PR_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	Planet_id INTEGER,
+	Resources_id INTEGER,
+	Price INTEGER,
+	CONSTRAINT fk_PR_Pl FOREIGN KEY (Planet_id) REFERENCES Planet (Planet_id),
+	CONSTRAINT fk_PR_RS FOREIGN KEY (Resources_id) REFERENCES Resources (Resources_id)
+);
+
+CREATE TABLE ShipResource (
+	SR_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	Ship_id INTEGER,
+	Resources_id INTEGER,
+	Amount INTEGER,
+	Bought_Price INTEGER,
+	CONSTRAINT fk_SR_Sh FOREIGN KEY (Ship_id) REFERENCES Ship (Ship_id),
+	CONSTRAINT fk_SR_Rs FOREIGN KEY (Resources_id) REFERENCES Resources (Resources_id)
+);
+
+CREATE TABLE ResourcesMedia (
+	RM_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	Resources_id INTEGER,
+	Media_id INTEGER,
+	CONSTRAINT fk_RM_R FOREIGN KEY (Resources_id) REFERENCES Resources (Resources_id),
+	CONSTRAINT fk_RM_M FOREIGN KEY (Media_id) REFERENCES Media (Media_id)
+);
+
+CREATE TABLE ShipMedia (
+	SM_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	Ship_id INTEGER,
+	Media_id INTEGER,
+	Reason INTEGER,
+	CONSTRAINT fk_SM_R FOREIGN KEY (Ship_id) REFERENCES Ship (Ship_id),
+	CONSTRAINT fk_SM_M FOREIGN KEY (Media_id) REFERENCES Media (Media_id)
+);
+
+CREATE TABLE PlanetMedia (
+	PM_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	Planet_id INTEGER,
+	Media_id INTEGER,
+	CONSTRAINT fk_PM_R FOREIGN KEY (Planet_id) REFERENCES Planet (Planet_id),
+	CONSTRAINT fk_PM_M FOREIGN KEY (Media_id) REFERENCES Media (Media_id)
+);
