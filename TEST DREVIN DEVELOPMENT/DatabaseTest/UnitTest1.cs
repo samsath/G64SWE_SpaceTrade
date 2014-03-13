5c919337c24c282e02c4d80a20a8bd9aee6b72cf
AbdullahAlbargi <brq_brq@hotmail.com>
2014-03-13 21:24:10 +0000

    yet we complite some classes but still need more work Rafif and Abdullah

---
+++ TEST DREVIN DEVELOPMENT/DatabaseTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataBaseClass;
+
+namespace DatabaseTest
+{
+    /// <summary>
+    /// the test from the lecture note 
+    /// create the unit test project then add class libarary named it DataBaseClass 
+    /// create class IDatabaseComponent
+    /// the test pass 
+    /// </summary>
+    [TestClass]
+    class DB_service
+    {
+        private IDatabaseComponent _dc;
+        public DB_service(IDatabaseComponent dc_in)
+        {
+	    _dc = dc_in;
+	    }
+    }
+
+}
+
---
+++ TEST DREVIN DEVELOPMENT/ObjectsClassLibarary/Objects.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+ */
+///adding the using from the file 
+
+namespace ObjectsClassLibarary
+{
+    public class Objects
+        /// create the class with the name object + s  so we dont use the key word for the C# 
+    {
+        private  int dt_Id;
+        private string title = "a";
+        private  string graphic = "b";
+
+
+        public int Dt_Id { get{return dt_Id;} set{dt_Id=value;} }
+
+        public string Title { get { return title; } set { title = value; } }
+
+        public string Graphic { get { return graphic; } set { graphic = value; } }
+    }
+}
