e700086075ecbf5d77a81c4116af993cd80e357e
Sam Hipwell <samhipwell@gmail.com>
2014-03-14 16:51:56 +0000

    Dan's game work all here

---
+++ SpaceGame/SpaceGame/SpaceGame/GameScreens/CharacterGeneratorScreen.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using XRpgLibrary;
+using XRpgLibrary.Controls;
+
+namespace SpaceGame.GameScreens
+{
+    public class CharacterGeneratorScreen : BaseGameState
+    {
+        #region Field Region
+
+        //LeftRightSelector genderSelector;
+        LeftRightSelector classSelector;
+        PictureBox backgroundImage;
+
+        //string[] genderItems = { "Male", "Female" };
+        string charName;
+        string[] classItems = { "Ship1", "Ship2", "Ship3", "Ship4" };
+
+        #endregion
+
+        #region Property Region
+        #endregion
+
+        #region Constructor Region
+
+        public CharacterGeneratorScreen(Game game, GameStateManager stateManager)
+            : base(game, stateManager)
+        {
+        }
+
+        #endregion
+
+        #region XNA Method Region
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            CreateControls();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            ControlManager.Update(gameTime, PlayerIndex.One);
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            GameRef.SpriteBatch.Begin();
+
+            base.Draw(gameTime);
+
+            ControlManager.Draw(GameRef.SpriteBatch);
+
+            GameRef.SpriteBatch.End();
+        }
+
+        #endregion
+
+        #region Method Region
+
+        private void CreateControls()
+        {
+            Texture2D leftTexture = Game.Content.Load<Texture2D>(@"GUI\leftarrowUp");
+            Texture2D rightTexture = Game.Content.Load<Texture2D>(@"GUI\rightarrowUp");
+            Texture2D stopTexture = Game.Content.Load<Texture2D>(@"GUI\StopBar");
+
+            backgroundImage = new PictureBox(
+                Game.Content.Load<Texture2D>(@"Backgrounds\titlescreen"),
+                GameRef.ScreenRectangle);
+            ControlManager.Add(backgroundImage);
+
+            Label label1 = new Label();
+
+            label1.Text = "Choose Your Ship";
+            label1.Size = label1.SpriteFont.MeasureString(label1.Text);
+            label1.Position = new Vector2((GameRef.Window.ClientBounds.Width - label1.Size.X) / 2, 150);
+
+            ControlManager.Add(label1);
+
+            //genderSelector = new LeftRightSelector(leftTexture, rightTexture, stopTexture);
+            //genderSelector.SetItems(genderItems, 125);
+            //genderSelector.Position = new Vector2(label1.Position.X, 200);
+
+            //ControlManager.Add(genderSelector);
+
+            classSelector = new LeftRightSelector(leftTexture, rightTexture, stopTexture);
+            classSelector.SetItems(classItems, 125);
+            classSelector.Position = new Vector2(label1.Position.X, 250);
+
+            ControlManager.Add(classSelector);
+
+            LinkLabel linkLabel1 = new LinkLabel();
+            linkLabel1.Text = "Use this Ship.";
+            linkLabel1.Position = new Vector2(label1.Position.X, 300);
+            linkLabel1.Selected += new EventHandler(linkLabel1_Selected);
+
+            ControlManager.Add(linkLabel1);
+
+            ControlManager.NextControl();
+        }
+
+        void linkLabel1_Selected(object sender, EventArgs e)
+        {
+            InputHandler.Flush();
+
+            StateManager.PopState();
+            StateManager.PushState(GameRef.GamePlayScreen);
+        }
+
+        #endregion
+    }
+}
