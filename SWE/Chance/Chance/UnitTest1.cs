ac956c0355b0667a4d41b293e74352b709ffc8c1
VIKI <qinyihuan@gmail.com>
2014-03-17 10:17:47 +0000

    check chance

---
+++ SWE/Chance/Chance/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace Chance
+{
+    [TestClass]
+    public class RandomEvent
+    {
+        [TestMethod]
+        public void checkchance()
+        {
+            Chancecheck sys = new Chancecheck();
+            int chance = sys.CheckEvent;
+            Assert.IsTrue(chance > 0);
+
+        }
+    }
+}
---
+++ SWE/Chance/ClassLibrary1/Chancecheck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Chancecheck
+    {
+        private int value;
+        public int CheckEvent
+        {
+            get {return value;}
+        }
+    }
+}
